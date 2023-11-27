582af102d849071027b82f27a47b469a8b855b60
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2023-11-27 18:14:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from rpunct import RestorePuncts
 import streamlit as st
 # The default language is 'english'
-rpunct = RestorePuncts()
+rpunct = RestorePuncts(use_cuda = False)
 r = rpunct.punctuate("Hello my name is Marcello how are you")
 st.write(r)
